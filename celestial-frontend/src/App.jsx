1dfe09ed8e11e99723ef4ac70ab9bdc27c8adf78
hypersonic3k <hypersonic3k@gmail.com>
2026-03-11 19:25:41 +0700

    Add TarotPage

--- celestial-frontend/src/App.jsx
+++ celestial-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Homepage from './modules/homepage/Homepage'
 import NumerologyPage from './modules/numerology/NumerologyPage'
 import AstrologyPage from './modules/astrology/AstrologyPage'
+import TarotPage from './modules/astrology/TarotPage'
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/numerology" element={<NumerologyPage />} />
         <Route path="/astrology" element={<AstrologyPage />} />
+        <Route path="/tarot" element={<TarotPage />} />
       </Routes>
     </BrowserRouter>
   )
